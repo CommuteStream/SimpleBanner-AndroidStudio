309a40d4e466c63c6b8b663e92f701d184e3f5e7
Samuel Pro <spro@transitchatter.com>
2015-03-03 23:19:16 -0600

    First Commit. Working on Sam's Note II

---
+++ app/src/main/java/com/commutestream/simplebannerstudio/MainActivity.java
@@ -0,0 +1,78 @@
+package com.commutestream.simplebannerstudio;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import com.commutestream.ads.*;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup some location stuff that we'll use in an example below
+        LocationManager locationManager = (LocationManager) this
+                .getSystemService(Context.LOCATION_SERVICE);
+        String NetworkLocationProvider = LocationManager.NETWORK_PROVIDER;
+        Location lastKnownLocation = locationManager
+                .getLastKnownLocation(NetworkLocationProvider);
+
+        CommuteStream.init();
+        //CommuteStream.setTesting();
+        CommuteStream.setApi_url("https://api.commutestreamdev.com:3000/");
+        CommuteStream.setTheme("dark");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        //AdRequest adRequest = new AdRequest.Builder().build();
+        AdRequest adRequest = new AdRequest.Builder()
+                .setLocation(lastKnownLocation).build();
+        mAdView.loadAd(adRequest);
+
+        /*
+		 * CommuteStream pays you more when you supply more information about
+		 * your users. Most importantly, what transit information they are viewing,
+		 * and also location, and when they do things like bookmarking or planning a trip.
+		 */
+
+        // Let's pretend that our app just displayed some arrival time information for two different
+        // train stops, and also displayed some alert information for the "Red" line route. This is
+        // how we would tell CommuteStream about it.
+        CommuteStream.trackingDisplayed("CTA", "Red", "41420"); // Addison
+        CommuteStream.trackingDisplayed("CTA", "Red", "41450"); // Chicago
+        CommuteStream.alertDisplayed("cta", "Red", null);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
